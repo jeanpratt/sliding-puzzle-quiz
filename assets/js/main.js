7802826f12e8654831a7aa9af41be18989eeef31
Jean Pratt <jean.dorothy.pratt@gmail.com>
2017-02-22 09:48:53 -0800

    Add Gameboard information through DOM
    To start coding the game's logic, I first had to add necessary
    elements and attributes through DOM manipulation in 'main.js'.
    
    1. The ninth tile was added
    2. All tiles now have a data-index to record location
    3. Each '.sliding-puzzle' has a relevant ID
    4. A separate array for the switching tiles was added

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,43 +5,68 @@
 	Project:	Sliding Puzzle Quiz (Swirl)
 	
 	Table of Contents:
-		1. @Utility
-		2. @Centered Text
-		3. @Game Logic
+		1. @Game Board
+		2. @Game Logic
 	
 ********/
 
 
 /********
 
-	@Utility
-	
-********/
-
-function $_class(ele) { return document.getElementsByClassName(ele); }
-
-
-/********
-
-	@Centered Text
+	@Game Board
 	
 ********/
 
 // Get puzzle
-var puzzles = $_class('sliding-puzzle');
+var puzzles = document.getElementsByClassName('sliding-puzzle');
 
 // In case there are multiple puzzles
 for (i = 0; i < puzzles.length; i++) {
 	
+	// Set ID of current puzzle
+	puzzleID = "sliding-puzzle-" + (i + 1);
+	puzzles[i].setAttribute("id", puzzleID)
+	
+	// Create blank tile
+	var tileBlank = document.createElement("li");
+	tileBlank.setAttribute("class", "tile");
+	tileBlank.dataset.index = 9;
+	puzzles[i].appendChild(tileBlank);
+	
 	// Select tiles
-	var tiles = puzzles[i].getElementsByClassName('tile');
+	var tiles = puzzles[i].getElementsByClassName('tile'),
+		numberTiles = parseInt(tiles.length) - 1;
 	
 	// For each text node within a tile, wrap the node within a span
-	for (j = 0; j < tiles.length; j++) {
+	// Add 'title' attribute for each tile
+	for (j = 0; j < numberTiles; j++) {
 		var tileNumber = tiles[j].firstChild,
 			tileSpan = document.createElement('span');
-			
+		
+		// Append text value to span and replace original child
 		tileSpan.appendChild(document.createTextNode(tileNumber.nodeValue));
 		tiles[j].replaceChild(tileSpan, tileNumber);
+		
+		// Add 'title' attribute
+		tiles[j].dataset.index = j + 1;
 	}
+	
+	playGame(puzzleID);
+}
+
+
+/********
+
+	@Game Logic
+	
+********/
+
+function playGame(puzzleID) {
+	var puzzleBoard = document.querySelector("#" + puzzleID),
+		puzzleTiles = { tiles: [] };
+		
+	for (i = 0; i < 8; i++) {
+		puzzleTiles.tiles.push(i + 1);
+	}
+	puzzleTiles.tiles.push("blank");
 }
